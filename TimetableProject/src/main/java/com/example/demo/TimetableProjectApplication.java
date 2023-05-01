bbe2e32cfb57485d79ee6cbecfb5f7704d0e1f6a
82102 <82102@BOOK-HMTM268N91>
2023-05-01 15:26:14 +0900

    초반 환경설정

---
+++ TimetableProject/src/main/java/com/example/demo/TimetableProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetableProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimetableProjectApplication.class, args);
+	}
+
+}
